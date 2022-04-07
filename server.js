7e8c71c6b8395160acea1b65e713db528a5e587d
selinyuksel <59738776+selinyuksel@users.noreply.github.com>
2022-04-06 23:12:42 -0400

    second first commit oops

--- server.js
+++ server.js
@@ -11,9 +11,12 @@
 // app.listen(4000);
 
 import express from 'express';
+import cors from 'cors';
 import userController   from "./controllers/user-controller.js";
+import tuitController from "./controllers/tuits-controller.js";
 
 const app = express();
-app.use(express.json());
+app.use(cors());
 userController(app);
-app.listen(4000);
+tuitController(app);
+app.listen(process.env.PORT || 4000);
